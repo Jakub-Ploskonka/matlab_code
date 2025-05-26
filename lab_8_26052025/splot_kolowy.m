97eadb2d2d5b45229c02132e5aedde2829f698fc
Jakub <jploskonka.kontakt@gmail.com>
2025-05-26 14:27:53 +0200

    lab_8_26052025

--- lab_8_26052025/splot_kolowy.m
+++ lab_8_26052025/splot_kolowy.m
@@ -28,3 +28,16 @@ function y = My_CircularConv(x, h)
         end
     end
 end
+
+
+
+
+function cc = my_cconv(a,b)
+
+L = length(a)+length(b)-1;
+
+a = [a, zeros([1,L-length(a)])];
+b = [b, zeros([1,L-length(b)])];
+
+cc = ifft(fft(a).*fft(b));
+cc = cc(1:L);
