19e8b0e35dc9709e9f663d935bc016a613a4d6df
Jakub-Ploskonka <jploskonka.kontakt@gmail.com>
2025-06-02 14:33:25 +0200

    Update zadanie_4.m

--- lab_8_26052025/zadanie_4.m
+++ lab_8_26052025/zadanie_4.m
@@ -123,6 +123,20 @@ for n=1:L
     y(n) = sum(b.*B)-sum(a(2:end).*A);
     A = [y(n),A(1:end-1)];
 end
+
+%My_odp_imp_2
+function h = My_odp_imp_2(b,a,L)
+
+h = zeros(1,L);
+B = zeros(1,length(b));%Delta kroneckera
+A = zeros(1,length(a)-1);%Poprzednie odpowiedzi systemu
+B(1) = 1;
+
+for n = 1:L
+   h(n) = sum(b.*B)-sum(a(2:end).*A);%b wsp przy x a przy y, przy drugim sum - bo y prenieśliśmy na drugą stronę
+   B = [0,B(1:end-1)];%Przesunięcie delty
+   A = [h(n),A(1:end-1)];%Przesunięcie odp systemu
+end
  
 
  
