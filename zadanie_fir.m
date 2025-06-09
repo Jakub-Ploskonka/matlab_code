ebedc1960dc63f80ddd8772928426960b98f5b66
Jakub-Ploskonka <jploskonka.kontakt@gmail.com>
2025-06-09 15:16:39 +0200

    Update zadanie_fir.m

--- zadanie_fir.m
+++ zadanie_fir.m
@@ -182,3 +182,124 @@ figure();
         title('FPZ (Filtr pasmowo zaporowy)');
         xlabel('f[Hz]');
         ylabel('Moduł');
+
+
+
+
+
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Autor Jakub Ploskonka
+% 09 06 2025
+% zadanie 2
+% metoda okien czasowych + DFT
+
+% Tabela z wartociami:
+%   'rectwin';     % prostokątne    21  1.1
+%   'bartlett';    % Bartlett’a     26  3.1
+%   'hann';        % von Hann’a     44  3.1
+%   'hamming';     % Hamming’a      53  3.3
+%   'blackman';    % Blackman’a     75  5.6
+%   'parzenwin';   % Parzen’a       56  7.0
+%   'flattopwin';  % flattop        115 9.7
+%   'chebwin';     % Chebyszew      70  4.6
+
+clc; clear; close all;
+
+fp = 1350; 
+fr = 1650; 
+Rr = 47; 
+fs = 8000;
+f0 = (fp+fr)/2;
+fir_1
+
+a = 3.3;
+R = ceil(a*fs/(fr-fp)); %R musi być większą bądź równą liczbą całkowitą dlatego zaokrąglamy w górę
+
+K = R+1; % ilosc probek
+n = 0 : R; 
+
+% metoda DFT
+M = 2* K;
+
+
+L = (M - 2) / 2;
+df = fs/M;
+f = 0 : df: M * df - df;
+
+o = floor(f0/df);
+
+fm = fs/M;
+Hm = zeros(1,M); 
+
+
+for m = 1 : M/2
+    if (m-1) * fm < f0
+        Hm(m) = 1;
+        Hm(M-m+1) = 1;
+    else 
+        Hm(m) = 0;
+    end
+end
+
+
+H2s = fftshift(ifft(Hm));
+
+H2o = H2s .* hamming(M)';
+
+[H2 F2]=freqz(H2o, 1,2^10,fs);
+
+fir_4
+
+
+figure(3)
+stem(n,H2s, LineWidth=2); grid on; hold on;
+title('impulsowa odpowiedź  filtru');
+xlabel('probki [n]'); % indeks próbkowania
+ylabel('Amplituda h[n]');
+legend('h[n]');
+
+
+figure(4)
+subplot(2,1,1)
+    plot(F2,(abs(H2)), 'r', LineWidth=2); grid on; hold on;
+    xline(fp, LineWidth=1);
+    xline(fr, LineWidth=1);
+    xlabel("Frequency (Hz)");
+    ylabel("Magnitude");
+subplot(2,1,2)
+    plot(F2,unwrap(angle(H2)), 'r', LineWidth=2); grid on; hold on;
+    xlabel("Frequency (Hz)");
+    ylabel("Phase");
+
+
+
+%Metoda okien czasowych
+n = 0 : R; % wektor probek indeksow czasowych
+H1s = 2 * (f0/fs) .* sinc(2 * (f0/fs) * (n - (R/2) )); % impulsowa odpowiedź  filtru dolnoprzepustowego, wyznaczona metodą próbkowania funkcji sinc
+H1o = H1s .* hamming(K)'; % odyfikację (przycięcie i wygładzenie) idealnej odpowiedzi impulsowej H1s za pomocą okna
+
+
+% Wykresy
+% [H2 F2] = freqz(H1o,1,2^10,fs);
+% fir_4
+
+% figure(3)
+% stem(n,H1s, LineWidth=2); grid on; hold on;
+% title('impulsowa odpowiedź  filtru');
+% xlabel('probki [n]'); % indeks próbkowania
+% ylabel('Amplituda h[n]');
+% legend('h[n], R = 53');
+% 
+% 
+% figure(4)
+% subplot(2,1,1)
+%     plot(F1,(abs(H1)), 'r', LineWidth=2); grid on; hold on;
+%     xline(fp, LineWidth=1);
+%     xline(fr, LineWidth=1);
+%     xlabel("Frequency (Hz)");
+%     ylabel("Magnitude");
+% subplot(2,1,2)
+%     plot(F1,unwrap(angle(H1)), 'r', LineWidth=2); grid on; hold on;
+%     xlabel("Frequency (Hz)");
+%     ylabel("Phase");
